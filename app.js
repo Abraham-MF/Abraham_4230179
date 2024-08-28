5997e1567710e936fe49f687e90177211a8836ab
Abraham-MF <159219728+Abraham-MF@users.noreply.github.com>
2024-08-27 20:08:54 -0600

    Update app.js

--- app.js
+++ app.js
@@ -7,6 +7,3 @@ let nacionalidad = prompt("¿cual es tu nacionalidad? ");
 console.log("tu nombre es: " + nombre);
 console.log("tu edad es: " + edad);
 console.log("eres de: " + nacionalidad);
-
-alert(nombre);
-
