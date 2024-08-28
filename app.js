6c818208c104e7f8a44ce06af14f2e71811a1d7d
Abraham-MF <abraham.morales2600@gmail.com>
2024-08-27 19:29:08 -0600

    Primer commit

---
+++ app.js
@@ -0,0 +1,12 @@
+let nombre = prompt("Ingresa tu nombre: ");
+
+let edad = prompt("¿Cual es tu edad? ");
+
+let nacionalidad = prompt("¿cual es tu nacionalidad? ");
+
+console.log("tu nombre es: " + nombre);
+console.log("tu edad es: " + edad);
+console.log("eres de: " + nacionalidad);
+
+alert(nombre);
+
