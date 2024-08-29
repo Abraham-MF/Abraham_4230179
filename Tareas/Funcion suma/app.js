0add4f20098132bd358445f9d09ff44de8b6b6c4
Abraham-MF <159219728+Abraham-MF@users.noreply.github.com>
2024-08-29 00:41:49 -0600

    Add files via upload

---
+++ Tareas/Funcion suma/app.js
@@ -0,0 +1,11 @@
+function suma(){
+let num1 = prompt("Ingresa el numero1: ");
+let num2 = prompt("pon el numero2: ");
+
+resultado = parseInt(num1) + parseInt(num2);
+
+
+console.log(resultado);
+
+}
+suma();
